f6e4760f256e1a92bb757eec8afb2bd446eaf397
Mickael0997 <158517216+Mickael0997@users.noreply.github.com>
2024-06-07 11:29:57 +0200

    adding assets and inserting the javascript folder and its script.js file

---
+++ JAVASCRIPT/scripts.js
@@ -0,0 +1,58 @@
+
+// Fonction évènements affichage des avis clients
+
+document.addEventListener('DOMContentLoaded', function() {
+
+// Animation pour les avis des visiteurs
+
+const avisList = document.getElementById('avis-list');
+const avis = [
+        { message: "Super expérience, les enfants ont adoré !", etoiles: 5 },
+        { message: "Beau parc mais un peu cher.", etoiles: 3 },
+        { message: "Très éducatif et bien entretenu.", etoiles: 4 },
+        { message: "Nous reviendrons avec plaisir !", etoiles: 5 },
+        { message: "Personnel très sympathique.", etoiles: 4 },
+        { message: "Bonne variété d'animaux.", etoiles: 4 },
+        { message: "Activités intéressantes pour les petits.", etoiles: 5 },
+        { message: "Parc très propre et bien organisé.", etoiles: 4 },
+        { message: "Excellente journée en famille.", etoiles: 5 },
+        { message: "Prix de la restauration un peu élevé.", etoiles: 3 }
+    ];
+
+// Initialisation et affichage de l'avis actuelle
+
+let avisIndex = 0;
+
+// Fonction qui reinitialise les avis    
+
+function afficherAvis() {
+        avisList.innerHTML = '';
+        const avisItem = document.createElement('li');
+        avisItem.textContent = `${avis[avisIndex].message} - ${avis[avisIndex].etoiles} étoiles`;
+        avisList.appendChild(avisItem);
+        avisIndex = (avisIndex + 1) % avis.length;
+    }
+
+// Créé une animation et affiche un nouvel avis toutes les 3 secondes
+
+    setInterval(afficherAvis, 3000); // Change l'avis toutes les 3 secondes
+
+/*
+   // Formulaire de connexion
+   const connexionBtn = document.getElementById("connexion");
+
+   connexionBtn.addEventListener('click', () => {
+     let fenetre = window.open("connexion.htm", 'Connexion', 'width=500, height=500');
+   
+   
+   });*/
+
+    // Formulaire de contact
+    const button = document.getElementById("contacte");
+
+    button.addEventListener('click', () => {
+      let fenetre = window.open("contact.htm", 'Contacte', 'width=500, height=500');
+      
+    
+    });
+});
